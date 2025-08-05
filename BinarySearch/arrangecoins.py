621f9b456ccaa2a7ae8d0115c8b54699c07d9edb
ramanan-26 <ramanansaravanan26@gmail.com>
2025-08-05 21:26:48 +0530

    Write better solutions

---
+++ BinarySearch/arrangecoins.py
@@ -0,0 +1,15 @@
+class Solution:
+    def arrangeCoins(self, n: int) -> int:
+        
+        i,j = 0,1
+        arr = []
+
+        while (i+j) <= n :
+            total = i+j 
+            if  == n:
+                return j
+            arr.append(sums)
+            i = sums
+            j += 1
+        arr.append(i)
+        return len(arr)-1 #Better Solution need to find
